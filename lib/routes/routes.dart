de1a44ebe94b8858a06da9d0637a1f9179c811f6
Albano Yanes <ajyanreyu@gmail.com>
2019-10-06 17:56:27 +0100

    Animated Container

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_components/pages/alert_page.dart';
+import 'package:flutter_components/pages/animated_container.dart';
 import 'package:flutter_components/pages/card_pages.dart';
 import 'package:flutter_components/pages/home_page.dart';
 import 'package:flutter_components/pages/avatar_page.dart';
@@ -9,6 +10,7 @@ Map<String, WidgetBuilder> getAppRoutes() {
     '/': (BuildContext context) => HomePage(),
     'alert': (BuildContext context) => AlertPage(),
     'avatar': (BuildContext context) => AvatarPage(),
-    'card': (BuildContext context) => CardPage()
+    'card': (BuildContext context) => CardPage(),
+    'animatedContainer': (BuildContext context) => AnimatedContainerPage()
   };
 }
