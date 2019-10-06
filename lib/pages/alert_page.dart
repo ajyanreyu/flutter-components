bf3fa17a30230de33c9e7cc767b891020a5a4da2
Albano Yanes <ajyanreyu@gmail.com>
2019-10-06 17:02:30 +0100

    Added alert dialog

--- lib/pages/alert_page.dart
+++ lib/pages/alert_page.dart
@@ -7,6 +7,40 @@ class AlertPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Alert Page'),
       ),
+      body: Center(
+          child: RaisedButton(
+        child: Text('show Alert'),
+        color: Colors.blue,
+        textColor: Colors.white,
+        shape: StadiumBorder(),
+        onPressed: () => _showAlert(context),
+      )),
     );
+  }
+
+  void _showAlert(BuildContext context) {
+    showDialog(
+        context: context,
+        barrierDismissible: true,
+        builder: (context) {
+          return AlertDialog(
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+            title: Text('Title'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[Text('Content'), FlutterLogo(size: 100)],
+            ),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('Cancel'),
+                onPressed: () => Navigator.of(context).pop(),
+              ),
+              FlatButton(
+                child: Text('Ok'),
+                onPressed: () {},
+              )
+            ],
+          );
+        });
   }
 }
