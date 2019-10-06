5be8f19c683b1da17987735f65e4efc3e9782816
Albano Yanes <ajyanreyu@gmail.com>
2019-10-06 18:39:38 +0100

    DateTimePicker basic configuration

--- lib/pages/input_page.dart
+++ lib/pages/input_page.dart
@@ -6,7 +6,11 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
-  String _name;
+  String _name = '';
+  String _email = '';
+  String _password = '';
+  String _date = '';
+  TextEditingController _inputFieldDateController = new TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +18,18 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(title: Text('Text Inputs')),
       body: ListView(
         padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
-        children: <Widget>[_createInputs(), Divider(), _createPerson()],
+        children: <Widget>[
+          _createInputs(),
+          Divider(),
+          _createdEmail(),
+          Divider(),
+          _createdPassword(),
+          Divider(),
+          _createdDate(context),
+          Divider(),
+          _createPerson(),
+          Divider(),
+        ],
       ),
     );
   }
@@ -34,9 +49,72 @@ class _InputPageState extends State<InputPage> {
     );
   }
 
+  Widget _createdEmail() {
+    return TextField(
+      keyboardType: TextInputType.emailAddress,
+      textCapitalization: TextCapitalization.sentences,
+      decoration: InputDecoration(
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+          counter: Text('Letters: ${_name.length}'),
+          hintText: 'email',
+          labelText: 'Email',
+          suffixIcon: Icon(Icons.lock),
+          icon: Icon(Icons.lock)),
+      onChanged: (value) => setState(() => _email = value),
+    );
+  }
+
   Widget _createPerson() {
     return ListTile(
       title: Text('Name: $_name'),
+      subtitle: Text('Email: $_email'),
     );
+  }
+
+  Widget _createdPassword() {
+    return TextField(
+      obscureText: true,
+      textCapitalization: TextCapitalization.sentences,
+      decoration: InputDecoration(
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+          counter: Text('Letters: ${_name.length}'),
+          hintText: 'Password',
+          labelText: 'Password',
+          suffixIcon: Icon(Icons.alternate_email),
+          icon: Icon(Icons.email)),
+      onChanged: (value) => setState(() => _password = value),
+    );
+  }
+
+  Widget _createdDate(BuildContext context) {
+    return TextField(
+      controller: _inputFieldDateController,
+      enableInteractiveSelection: false,
+      textCapitalization: TextCapitalization.sentences,
+      decoration: InputDecoration(
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+          counter: Text('Letters: ${_name.length}'),
+          hintText: 'Day of birth',
+          labelText: 'Day of birth',
+          suffixIcon: Icon(Icons.perm_contact_calendar),
+          icon: Icon(Icons.calendar_today)),
+      onTap: () {
+        FocusScope.of(context).requestFocus(new FocusNode());
+        _selectDate(context);
+      },
+    );
+  }
+
+  void _selectDate(BuildContext context) async {
+    DateTime picked = await showDatePicker(
+      context: context,
+      initialDate: new DateTime.now(),
+      firstDate: new DateTime(2018),
+      lastDate: new DateTime(2050),
+    );
+    if (picked != null) {
+      _date = picked.toString();
+      _inputFieldDateController.text = _date;
+    }
   }
 }
